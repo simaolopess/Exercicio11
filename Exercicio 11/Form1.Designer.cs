793b16c3cd226ca0ca52da8f564b7d023b5a4586
Aluno-AEDS <Aluno-AEDS@DESKTOP-OJGD1EU>
2023-11-21 17:22:39 +0000

    mudar cor da letra e mudar o tipo de letra

--- Exercicio 11/Form1.Designer.cs
+++ Exercicio 11/Form1.Designer.cs
@@ -28,31 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            bngnToolStripMenuItem = new ToolStripMenuItem();
+            mudarCorELetraToolStripMenuItem = new ToolStripMenuItem();
+            letraToolStripMenuItem = new ToolStripMenuItem();
+            corToolStripMenuItem = new ToolStripMenuItem();
             button1 = new Button();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 25);
+            label1.TabIndex = 1;
+            label1.Text = "SISI CASMURRA";
+            label1.Click += label1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { bngnToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1260, 33);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // bngnToolStripMenuItem
+            // 
+            bngnToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mudarCorELetraToolStripMenuItem });
+            bngnToolStripMenuItem.Name = "bngnToolStripMenuItem";
+            bngnToolStripMenuItem.Size = new Size(163, 29);
+            bngnToolStripMenuItem.Text = "mudar cor e letra";
+            bngnToolStripMenuItem.Click += bngnToolStripMenuItem_Click;
+            // 
+            // mudarCorELetraToolStripMenuItem
+            // 
+            mudarCorELetraToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { letraToolStripMenuItem, corToolStripMenuItem });
+            mudarCorELetraToolStripMenuItem.Name = "mudarCorELetraToolStripMenuItem";
+            mudarCorELetraToolStripMenuItem.Size = new Size(249, 34);
+            mudarCorELetraToolStripMenuItem.Text = "mudar cor e letra";
+            // 
+            // letraToolStripMenuItem
+            // 
+            letraToolStripMenuItem.Name = "letraToolStripMenuItem";
+            letraToolStripMenuItem.Size = new Size(148, 34);
+            letraToolStripMenuItem.Text = "letra";
+            letraToolStripMenuItem.Click += letraToolStripMenuItem_Click;
+            // 
+            // corToolStripMenuItem
+            // 
+            corToolStripMenuItem.Name = "corToolStripMenuItem";
+            corToolStripMenuItem.Size = new Size(148, 34);
+            corToolStripMenuItem.Text = "cor";
+            corToolStripMenuItem.Click += corToolStripMenuItem_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(149, 94);
+            button1.Location = new Point(1058, 191);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Size = new Size(176, 109);
+            button1.TabIndex = 3;
+            button1.Text = "pagina 2";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Red;
+            ClientSize = new Size(1260, 359);
             Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem bngnToolStripMenuItem;
+        private ToolStripMenuItem mudarCorELetraToolStripMenuItem;
+        private ToolStripMenuItem letraToolStripMenuItem;
+        private ToolStripMenuItem corToolStripMenuItem;
         private Button button1;
     }
 }
--- Exercicio 11/Form1.cs
+++ Exercicio 11/Form1.cs
@@ -5,6 +5,52 @@ namespace Exercicio_11
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bngnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void corToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                label1.ForeColor = colorDialog.Color;
+
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.Font = fontDialog.Font;
+            }
+
+
+        }
+
+        private void letraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.Font = fontDialog.Font;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form frm_2 = new Form1();
+            frm_2.ShowDialog();
+
         }
     }
 }
